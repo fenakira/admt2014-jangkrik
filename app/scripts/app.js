d490a6eead12c36cdba6a2c5cd74b25c8b2d1be3
Ashley Andrian <fenakira@gmail.com>
2014-11-23 07:49:34 +0700

    fix boilerplate

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,30 @@
-angular.module('jangkrikApp', [])
+var app = angular.module('jangkrikApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', 
+    function($routeProvider, $locationProvider) {
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'templates/home.html',
+                controller: 'HomeCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'templates/about.html',
+                controller: 'AboutCtrl'
+            })
+            .when('/behaviour', {
+                templateUrl: 'templates/behaviour.html',
+                controller: 'BehaviourCtrl'
+            })
+            .when('/404', {
+                template: '<h1>404 | Not Found</h1>'
+            })
+            .otherwise({ redirectTo: '/404' });
+
+        // $locationProvider.html5Mode({
+        //     enabled: true,
+        //     requireBase: false
+        // });
+
+    }
+]);
---
+++ app/scripts/controllers.js
@@ -0,0 +1,10 @@
+app
+    .controller('HomeCtrl', ['$scope', function ($scope) {
+        $scope.title = 'Home';
+    }])
+    controller('AboutCtrl', ['$scope', function ($scope) {
+        $scope.title = 'ABout';
+    }])
+    controller('BehaviourCtrl', ['$scope', function ($scope) {
+        $scope.title = 'Behaviour';
+    }]);
