b1226558d6236d322ace89458791de37cefaa6f1
Ashley Andrian <fenakira@gmail.com>
2014-11-28 19:48:49 +0700

    Overhaul design into flat design (initialize design)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,50 +1,6 @@
 var  APPNAME = 'jangkrikApp',
      app     = angular.module(APPNAME, ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider', 
-    function($routeProvider, $locationProvider) {
-
-        $routeProvider
-            .when('/', {
-                templateUrl: 'templates/home.html',
-                controller: 'HomeCtrl'
-            })
-            .when('/home', {
-                redirectTo: '/'
-            })
-            .when('/about', {
-                templateUrl: 'templates/about.html',
-                controller: 'AboutCtrl'
-            })
-            .when('/behaviour', {
-                templateUrl: 'templates/behaviour.html',
-                controller: 'BehaviourCtrl'
-            })
-            .when('/culture', {
-                templateUrl: 'templates/culture.html',
-                controller: 'CultureCtrl'
-            })
-            .when('/gallery', {
-                templateUrl: 'templates/gallery.html',
-                controller: 'GalleryCtrl'
-            })
-            .when('/contact', {
-                templateUrl: 'templates/contact.html',
-                controller: 'ContactCtrl'
-            })
-            .when('/404', {
-                template: '404 | Not Found'
-            })
-            .otherwise({ redirectTo: '/404' });
-
-        // $locationProvider.html5Mode({
-        //     enabled: true,
-        //     requireBase: false
-        // });
-
-    }
-]);
-
 angular.element(document).ready(function() {
     angular.bootstrap(document, [APPNAME]);
 });
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,22 +1,7 @@
 app
-    .controller('HomeCtrl', ['$rootScope', function ($rootScope) {
-        $rootScope.title = 'Home';
-    }])
-    .controller('AboutCtrl', ['$rootScope', function ($rootScope) {
-        $rootScope.title = 'About';
-    }])
-    .controller('BehaviourCtrl', ['$rootScope', function ($rootScope) {
-        $rootScope.title = 'Behaviour';
-    }])
-    .controller('CultureCtrl', ['$rootScope', function ($rootScope) {
-        $rootScope.title = 'Culture';
-    }])
-    .controller('GalleryCtrl', ['$rootScope', function ($rootScope) {
-        $rootScope.title = 'Gallery';
-    }])
-    .controller('ContactCtrl', ['$rootScope', function ($rootScope) {
-        $rootScope.title = 'Contact';
-    }])
-    .controller('HeaderCtrl', ['$rootScope', function ($rootScope){
-        $rootScope.navigations = ['Home', 'About', 'Behaviour', 'Culture', 'Gallery', 'Contact'];
+    .controller('GlobalCtrl', ['$rootScope', '$scope', function ($rootScope, $scope){
+        $scope.navigations = ['Home', 'About', 'Behaviour', 'Culture', 'Gallery', 'Contact'];
+        $scope.changeTitle = function(index) {
+            $rootScope.title = $scope.navigations[index];
+        }
     }])
